6ed4a0cb3d8547b323917c14b6851bb139e3f4b8
Natalibro <brodka@mail.ru>
2024-02-27 20:29:44 +0300

    task2

---
+++ task2/Program.cs
@@ -0,0 +1,14 @@
+﻿// из трех задач делаем одну применяя функцию
+
+void PrintSquares (int limit)
+{
+    int i = 1;
+    while(i <= limit)
+    {
+        Console.Write($"{i*i} ");
+        i++;
+    }
+}
+PrintSquares(5);
+PrintSquares(10);
+PrintSquares(15);
